4d106ffb2ea7d139dbb1879930ad96894b0f455a
Enrique Garcia <github@lemambo.com>
2022-10-25 23:30:41 +0200

    Update platforms as this needs UIKit

--- Package.swift
+++ Package.swift
@@ -1,9 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "Ruka",
+    platforms: [.iOS(.v15),
+                .watchOS(.v8),
+                .tvOS(.v15),
+                .macCatalyst(.v13)],
     products: [
         .library(
             name: "Ruka",
