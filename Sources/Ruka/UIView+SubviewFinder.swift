5753f7a299c4d67df6dea90e99a0549234bef9d4
Enrique Garcia <github@lemambo.com>
2024-03-31 18:27:08 +0200

    add method to check if a view is tappable

--- Sources/Ruka/UIView+SubviewFinder.swift
+++ Sources/Ruka/UIView+SubviewFinder.swift
@@ -21,4 +21,54 @@ extension UIView {
     private var recursiveSubviews: [UIView] {
         return subviews + subviews.flatMap { $0.recursiveSubviews }
     }
+
+    func isTappable() -> Bool {
+        let tappable = isTappable(in: self.bounds)
+        return tappable
+    }
+
+    func isTappable(in viewBounds: CGRect) -> Bool {
+        let tapPoint = tappablePoint(in: viewBounds)
+        return tapPoint != nil ? true : false
+    }
+
+    func tappablePoint(in rect: CGRect) -> CGPoint? {
+        guard let window = self.window else {
+            return nil
+        }
+        // Convert the rect from the view's coordinate system to the window's coordinate system
+        let frame = window.convert(rect, from: self)
+
+        //  TL                               TR
+        //    +-----------------------------+
+        //    |                             |
+        //    |                             |
+        //    |              + C            |
+        //    |                             |
+        //    |                             |
+        //    +-----------------------------+
+        //  BL                               BR
+        let pointsToTest = [
+            CGPoint(x: frame.midX, y: frame.midY),             // C  Center
+            CGPoint(x: frame.minX + 1.0, y: frame.minY + 1.0), // TL Top left
+            CGPoint(x: frame.maxX - 1.0, y: frame.minY + 1.0), // TR Top right
+            CGPoint(x: frame.minX + 1.0, y: frame.maxY - 1.0), // BL Bottom left
+            CGPoint(x: frame.maxX - 1.0, y: frame.maxY - 1.0)  // BR Bottom right
+        ]
+
+        for tapPoint in pointsToTest {
+            if let hitView = window.hitTest(tapPoint, with: nil){
+                if hitView === self {
+                    return window.convert(tapPoint, to: self)
+                } else {
+                    if hitView.isDescendant(of: self) {
+                        return window.convert(tapPoint, to: self)
+                    } else {
+                    }
+                }
+            }
+        }
+
+        return nil
+    }
 }
--- Sources/Ruka/UIViewController+ext.swift
+++ Sources/Ruka/UIViewController+ext.swift
@@ -15,11 +15,20 @@ import AppKit
 
 public extension UIViewController {
     func getViewBy<T: UIView>(_ identifier: String,
+                              tappable checkTappable: Bool = true,
                               file: StaticString = #filePath,
                               line: UInt = #line,
                               failureBehavior: FailureBehavior = .failTest) throws -> T? {
         let views = self.view.findViews(subclassOf: T.self)
-        let view = views.first(where: { $0.isIdentifiable(by: identifier, in: self) })
+        let view = views.first(where: {
+            if checkTappable,
+               $0.isIdentifiable(by: identifier, in: self) {
+                let tappable = $0.isTappable()
+                return tappable
+            } else {
+                return $0.isIdentifiable(by: identifier, in: self)
+            }
+        })
 
         if view == nil, failureBehavior != FailureBehavior.doNothing {
             try failureBehavior.failOrRaise("Could not find view with '\(identifier)'.",
