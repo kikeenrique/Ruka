ab1ecf62bbad6ed367a58ed6e72eaf0b31719da5
Enrique Garcia <github@lemambo.com>
2024-04-02 02:02:19 +0200

    fix some tests that were failing because screen was too small
    + After removing launch screen storyboard, screen was launched in and old compatibility mode.
      Fixed it declaring a minimal launch screen via storyboard.
      Check https://developer.apple.com/documentation/xcode/specifying-your-apps-launch-screen/

--- Tests/Host App/MainViewController.swift
+++ Tests/Host App/MainViewController.swift
@@ -9,7 +9,19 @@ import AppKit
 class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .systemGray
+
+        let subview = UIView()
+        subview.backgroundColor = .red
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(subview)
+
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            subview.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            subview.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            subview.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            subview.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
     }
 }
