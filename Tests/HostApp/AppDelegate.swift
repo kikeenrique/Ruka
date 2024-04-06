bbcd8ec141890aea0ab11988f1c7c3846b3b0dd5
Enrique Garcia <github@lemambo.com>
2024-04-06 20:36:15 +0200

    recreate host app

--- Tests/HostApp/AppDelegate.swift
+++ Tests/HostApp/AppDelegate.swift
@@ -1,3 +1,10 @@
+//
+//  AppDelegate.swift
+//  HostApp
+//
+//  Created by Enrique Garcia Alvarez on 6/4/24.
+//
+
 #if canImport(UIKit)
 import UIKit
 #endif
@@ -5,16 +12,21 @@ import UIKit
 import AppKit
 #endif
 
-
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication, 
+                     configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, 
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
--- Tests/HostApp/SceneDelegate.swift
+++ Tests/HostApp/SceneDelegate.swift
@@ -1,3 +1,10 @@
+//
+//  SceneDelegate.swift
+//  HostApp
+//
+//  Created by Enrique Garcia Alvarez on 6/4/24.
+//
+
 #if canImport(UIKit)
 import UIKit
 #endif
@@ -5,14 +12,31 @@ import UIKit
 import AppKit
 #endif
 
-
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
     }
 }
