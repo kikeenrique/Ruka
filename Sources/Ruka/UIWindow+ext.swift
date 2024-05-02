1ff8a473e5c97e7298046ea5ff11e921f36c0ea2
Enrique Garcia <github@lemambo.com>
2024-05-02 21:40:02 +0200

    relocate code

--- Sources/Ruka/UIWindow+ext.swift
+++ Sources/Ruka/UIWindow+ext.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import UIKit
-import os.log
 
 extension UIWindow {
     // MARK: UILabel
@@ -93,64 +92,5 @@ extension UIWindow {
         return try rootViewController?.visibleViewController?.getViewBy(identifier,
                                                                         tappable: tappable,
                                                                         failureBehavior: failureBehavior)
-    }
-}
-
-extension UIWindow {
-    func areAnimationsFinished() -> Bool {
-        let views = getAllViews()
-        let logger = Logger(subsystem: "App", category: "App")
-        let animations: Bool
-        if views.isEmpty {
-            animations = false
-        } else {
-            animations = views.allSatisfy { view in
-                if view.layer.hasFiniteAnimations(logged: true) {
-                    return true
-                } else {
-                    return false
-                }
-            }
-        }
-        logger.debug("\(#function) views:\(views.count) animations:\(String(describing: animations))")
-        return !animations
-    }
-
-    func getAllViews() -> [UIView] {
-        var allViews = [UIView]()
-
-        if let rootViewController = self.rootViewController {
-            traverseSubviews(of: rootViewController.view,
-                             accumulating: &allViews)
-            // Include views from child view controllers
-            getChildViewControllers(rootViewController).forEach { viewController in
-                traverseSubviews(of: viewController.view,
-                                 accumulating: &allViews)
-            }
-        }
-
-        return allViews
-    }
-
-    private func traverseSubviews(of view: UIView,
-                                  accumulating allViews: inout [UIView]) {
-        allViews.append(view)
-        view.subviews.forEach { subview in
-            traverseSubviews(of: subview,
-                             accumulating: &allViews)
-        }
-    }
-
-    private func getChildViewControllers(_ viewController: UIViewController) -> [UIViewController] {
-        var viewControllers = [UIViewController]()
-        viewControllers.append(contentsOf: viewController.children)
-        viewController.children.forEach { childViewController in
-            viewControllers.append(contentsOf: getChildViewControllers(childViewController))
-        }
-        if let presentedViewController = viewController.presentedViewController {
-            viewControllers.append(presentedViewController)
-            viewControllers.append(contentsOf: getChildViewControllers(presentedViewController))
-        }
-        return viewControllers
     }
 }
